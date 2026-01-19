619d008ddeb03571ea9763d08197b6f364ce1fe5
ferreiraluisa <02luisafsouza@gmail.com>
2026-01-19 16:24:46 +0100

    changing data preparation

--- run_read_h36m.sh
+++ run_read_h36m.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#SBATCH --partition=A40devel     # GPU partition
-#SBATCH --time=1:00:00           # max runtime (HH:MM:SS)
+#SBATCH --partition=A40short     # GPU partition
+#SBATCH --time=8:00:00           # max runtime (HH:MM:SS)
 #SBATCH --gpus=1                 # number of GPUs
 #SBATCH --ntasks=1               # number of CPU tasks
-#SBATCH --cpus-per-task=4        # number of CPU cores per task (optional)
-#SBATCH --mem=16G                # RAM (optional)
 #SBATCH --output=run_read_h36m_%j.out
 #SBATCH --error=run_read_h36m_%j.err
 
