87de0850fbd7719df9a53d98f098974295e49b73
ferreiraluisa <02luisafsouza@gmail.com>
2026-02-28 14:11:34 +0100

    fixing joint

--- src/dataset_features.py
+++ src/dataset_features.py
@@ -57,7 +57,7 @@ class Human36MFeatureClips(Dataset):
         joints2d = d["joints2d"]
         K = d["K"]
 
-        root = joints3d[:, :, 0:1, :]      # (T, 1, 3)
+        root = joints3d[:, 0:1, :]       # (T, 1, 3)
         joints3d_norm = joints3d - root     # root-relative
 
         if self.test_set:
