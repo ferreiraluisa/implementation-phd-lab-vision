2ec416ef34952710ddef25313369fc45b88a1dea
ferreiraluisa <02luisafsouza@gmail.com>
2026-02-28 14:05:34 +0100

    data loading faster

--- src/train.py
+++ src/train.py
@@ -342,6 +342,9 @@ def main():
         shuffle=True,
         num_workers=args.num_workers,
         drop_last=True,
+        pin_memory=True,
+        prefetch_factor=4 if args.num_workers > 0 else None,
+        persistent_workers=True if args.num_workers > 0 else False,
     )
     val_loader = DataLoader(
         val_set,
@@ -349,6 +352,9 @@ def main():
         shuffle=False,
         num_workers=max(1, args.num_workers // 2),
         drop_last=False,
+        pin_memory=True,
+        prefetch_factor=2 if args.num_workers > 0 else None,
+        persistent_workers=True if args.num_workers > 0 else False,
     )
 
     model = PHD(joints_num=JOINTS_NUM)
