5e0a7ffd58b7491e38da8ea6a57de4342700a6ba
ferreiraluisa <02luisafsouza@gmail.com>
2026-03-01 12:42:20 +0100

    fixing sh train

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=A40short    # GPU partition
+#SBATCH --partition=A100short    # GPU partition
 #SBATCH --time=8:00:00           # max runtime (HH:MM:SS)
-#SBATCH --gpus=2                 # number of GPUs
+#SBATCH --gpus=4                 # number of GPUs
 #SBATCH --ntasks=1               # number of CPU tasks
 #SBATCH --output=logs/train%j.out
 #SBATCH --error=logs/train%j.err
@@ -16,9 +16,8 @@ nvidia-smi -l 1800 &
 
 python -u src/train.py \
   --seq-len 40 \
-  --root /home/s26ldeso/Human3.6M_preprocessed_resnet_features \
-  --batch-size 16 \
+  --root /home/s26ldeso/Human3.6M_resnet_data \
+  --batch-size 32 \
   --lr 1e-4 \
   --epochs 50 \
-  --stride 5 \
   --outdir runs/phase1
